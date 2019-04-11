e4f97584263cd76f32830aa44ff4023ef756f064
IsadoraTeles <isadoratelesdecastro@gmail.com>
2019-04-11 11:39:45 +0200

    move visible files to public directory, creation of express server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 
 // importing the module that you're using
-var express = require('express');
+var express = require('express'); // require access to 'express'
+
+var app = express(); // create the app, making 'express' application
+
+var server = app.listen (3000);
+
+app.use(express.static('public')); // see whats in the public directory
 
 // test code
 console.log("socket server running");
