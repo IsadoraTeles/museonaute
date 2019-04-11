77a15facb51bd9a931a5b5947390a5ae857ead70
IsadoraTeles <isadoratelesdecastro@gmail.com>
2019-04-11 15:02:10 +0200

    node.js client updates

--- public/sketch.js
+++ public/sketch.js
@@ -1,52 +1,42 @@
-var serial; // variable to hold an instance of the serialport library
-var portName = '/dev/cu.usbmodem1411'; // fill in your serial port name here
-var inData;
-//var outByte = 0; // for incoming serial data                      // for outgoing data
-var potentiometer;
-// var mapped;
-// var inString;
-// var pushbutton = 1;
-// var mapped = map(potentiometer, 0, 255, 0, 360);
 
-function setup() {
-  noCanvas(); // make the canvas
-  serial = new p5.SerialPort(); // make a new instance of the serialport library
-  serial.on('data', serialEvent); // callback for when new data arrives
-  serial.on('error', serialError); // callback for errors
-  serial.on('open', gotOpen);
-  serial.open(portName); // open a serial port
+
+// IO
+var socket;
+
+
+function setup() 
+{
+  createCanvas(720, 400);
+
+  // IO
+  socket = io.connect('http://localhost:3000');
+  socket.on('mouse', newDrawing);
+};
+      
+function newDrawing(data)
+{
+  fill(0,255,0);
+  ellipse(data.x, data.y, 20, 20);
 }
 
-// Connected to our serial device
-function gotOpen() {
-  print("Serial Port is open!");
-  serial.write(0);
-}
-
-function serialEvent() {
-  // // read a byte from the serial port:
-  // var inByte = serial.read();
-  // // store it in a global variable:
-  // inData = inByte;
-  let inString = serial.readLine()
-  // print(inString);
-
-  if (inString.length > 0) {
-    var d = split(inString, ',');
-    potentiometer = Number(d[0]);
-    var mapped = map(potentiometer, 0, 1023, 0, 255);
-    var integer = nfc(mapped,0,0);
-    threeSerialData(integer);
-    //document.dispatchEvent(new CustomEvent("serialData", { v: mapped }));
-    // pushbutton = Number(d[1]);
-    
-  }
+function draw() 
+{
+  //background(200);
   
-  serial.write(0);
 }
 
-function serialError(err) {
-  print('Something went wrong with the serial port. ' + err);
+// Start it up
+function mouseDragged() 
+{
+  fill(255,0,0);
+  ellipse(mouseX, mouseY, 5, 5);
+  var data = 
+  {
+    x : mouseX,
+    y : mouseY
+  }
+  socket.emit('mouse', data); // name and data
+  console.log('Sending : '+ mouseX + ',' + mouseY);
+  return false;
 }
 
-function draw() {}
--- server.js
+++ server.js
@@ -10,3 +10,21 @@ app.use(express.static('public')); // see whats in the public directory
 
 // test code
 console.log("socket server running");
+
+var socket = require('socket.io');
+
+// keep track of io
+var io = socket(server);
+io.sockets.on('connection', newConnection);
+function newConnection(socket) // triggered with new socket connection
+{
+    console.log('new connection' + socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data)
+    {
+        socket.broadcast.emit('mouse', data)
+        console.log('Server receiving : '+ data.x + ', ' + data.y);
+    }
+}
