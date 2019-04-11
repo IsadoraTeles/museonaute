8829419990a2c826c4bdaf5801768b4537005512
IsadoraTeles <isadoratelesdecastro@gmail.com>
2019-04-11 18:01:08 +0200

    implementation of node osc

--- public/sketch.js
+++ public/sketch.js
@@ -2,41 +2,56 @@
 
 // IO
 var socket;
+var x = 100;
+var y = 100;
 
+function receiveOsc(address, value) 
+{
+	console.log("received OSC: " + address + ", " + value);
+	
+	if (address == '/test') {
+		x = value[0];
+		y = value[1];
+	}
+}
+
+function sendOsc(address, value) 
+{
+	socket.emit('message', [address].concat(value));
+}
+
+function setupOsc(oscPortIn, oscPortOut) 
+{
+	var socket = io.connect('http://192.168.1.14:8081', { port: 8000, rememberTransport: false });
+	socket.on('connect', function() {
+		socket.emit('config', {	
+			server: { port: oscPortIn,  host: '192.168.1.14'},
+			client: { port: oscPortOut, host: '192.168.1.14'}
+		});
+	});
+	socket.on('message', function(msg) {
+		if (msg[0] == '#bundle') {
+			for (var i=2; i<msg.length; i++) {
+				receiveOsc(msg[i][0], msg[i].splice(1));
+			}
+		} else {
+			receiveOsc(msg[0], msg.splice(1));
+		}
+	});
+}
 
 function setup() 
 {
   createCanvas(720, 400);
-
-  // IO
-  socket = io.connect('http://localhost:3000');
-  socket.on('mouse', newDrawing);
+  setupOsc(12000, 3334);
 };
       
-function newDrawing(data)
-{
-  fill(0,255,0);
-  ellipse(data.x, data.y, 20, 20);
-}
 
 function draw() 
 {
-  //background(200);
-  
+  fill(255,0,0);
+  ellipse(x, y, 15, 15);
 }
 
-// Start it up
-function mouseDragged() 
-{
-  fill(255,0,0);
-  ellipse(mouseX, mouseY, 5, 5);
-  var data = 
-  {
-    x : mouseX,
-    y : mouseY
-  }
-  socket.emit('mouse', data); // name and data
-  console.log('Sending : '+ mouseX + ',' + mouseY);
-  return false;
-}
+
 
--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 
-// importing the module that you're using
-var express = require('express'); // require access to 'express'
+var osc = require('node-osc');
+var io = require('socket.io')(8081);
 
-var app = express(); // create the app, making 'express' application
 
-var server = app.listen (3000);
+var oscServer, oscClient;
 
-app.use(express.static('public')); // see whats in the public directory
+var isConnected = false;
 
-// test code
-console.log("socket server running");
-
-var socket = require('socket.io');
-
-// keep track of io
-var io = socket(server);
-io.sockets.on('connection', newConnection);
-function newConnection(socket) // triggered with new socket connection
-{
-    console.log('new connection' + socket.id);
-
-    socket.on('mouse', mouseMsg);
-
-    function mouseMsg(data)
-    {
-        socket.broadcast.emit('mouse', data)
-        console.log('Server receiving : '+ data.x + ', ' + data.y);
-    }
-}
+io.sockets.on('connection', function (socket) {
+	console.log('connection');
+	socket.on("config", function (obj) {
+		isConnected = true;
+    	oscServer = new osc.Server(obj.server.port, obj.server.host);
+	    oscClient = new osc.Client(obj.client.host, obj.client.port);
+	    oscClient.send('/status', socket.sessionId + ' connected');
+		oscServer.on('message', function(msg, rinfo) {
+			socket.emit("message", msg);
+		});
+		socket.emit("connected", 1);
+	});
+ 	socket.on("message", function (obj) {
+		oscClient.send.apply(oscClient, obj);
+  	});
+	socket.on('disconnect', function(){
+		if (isConnected) {
+			oscServer.kill();
+			oscClient.kill();
+		}
+  	});
+});
